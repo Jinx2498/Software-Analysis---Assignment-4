f4cde92f369299d5393ce6590fec96e557b37f1b
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-12-03 18:17:26 -0500

    Update DrawingArea.java

--- DrawingArea.java
+++ DrawingArea.java
@@ -18,7 +18,9 @@ public class DrawingArea extends JLabel {
     
     private State state = State.Start;
     
-    private ArrayList<Rectangle> shapes = new ArrayList<Rectangle>();
+    private ArrayList<Rectangle> shapesR = new ArrayList<Rectangle>();
+	private ArrayList<Oval> shapesO = new ArrayList<Oval>();
+	private ArrayList<Triangle> shapesT = new ArrayList<Triangle>();
     
     public DrawingArea() {
     	this.setBackground(Color.BLACK);
@@ -51,18 +53,48 @@ public class DrawingArea extends JLabel {
     	if (x1 == 0 && x2 == 0 && y1 == 0 && y2 == 0) return;
     	
     	if (state == State.Moving){
-    		for (Rectangle s: shapes) {
+
+    		for (Rectangle s: shapesR) {
 	    		s.move(x1, y1, x2, y2);
 	    		s.paint(g);
     		}
-    	}
-    	else if (state == State.Rectangle) {
-        	shapes.add(new Rectangle(x1, y1, x2-x1, y2-y1, Color.BLUE));
-    	}
 
-    	for (Rectangle s: shapes){
+			for (Oval s: shapesO) {
+	    		s.move(x1, y1, x2, y2);
+	    		s.paint(g);
+    		}
+
+			for (Triangle s: shapesT) {
+	    		s.move(x1, y1, x2, y2);
+	    		s.paint(g);
+    		}
+
+    	} else if (state == State.Rectangle) {
+
+        	shapesR.add(new Rectangle(x1, y1, x2-x1, y2-y1, Color.BLUE));
+
+    	} else if (state == State.Oval) {
+
+			shapesO.add(new Oval(x1, y1, x2-x1, y2-y1, Color.RED));
+
+		} else if (state == State.Triangle) {
+
+			shapesT.add(new Triangle(x1, y1, x2-x1, y2-y1, Color.GREEN));
+
+		}
+
+    	for (Rectangle s: shapesR){
     		s.paint(g);
     	}
+
+		for (Oval s: shapesO){
+    		s.paint(g);
+    	}
+
+		for (Triangle s: shapesT){
+    		s.paint(g);
+    	}
+
     }
     
     public void changeState(State newState) {
